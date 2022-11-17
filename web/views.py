816e052e4602554ae320d8104b9587cd4694085f
SajithaRaveendran <sajitharaveendran2111.com>
2022-11-17 14:09:59 +0530

    adding filter

--- web/views.py
+++ web/views.py
@@ -3,10 +3,12 @@ from datetime import datetime, timedelta
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
-from posts.models import Event
+from posts.models import Event, Customer
+
 
 def index(request):
-    events = Event.objects.filter(is_deleted=False)
+    events = Event.objects.filter(is_deleted=False,single_time=True)
+    customers = Customer.objects.all()
     print(datetime.now().date())
 
     sortby = request.GET.get('sort', '')
@@ -23,12 +25,19 @@ def index(request):
         dt = datetime.strptime(today.strftime("%Y%m%d"), '%Y%m%d')
         start = dt - timedelta(days=dt.weekday())
         end = start + timedelta(days=6)
-        
-        events = Event.objects.filter(event_date__range=[start,end])
 
+        events = Event.objects.filter(event_date__range=[start, end])
+
+    search_customers = request.GET.getlist("user")
+    print(search_customers)
+
+    if search_customers:
+        events = events.filter(user__in=search_customers)
 
     context = {
         "title": "Home Page",
-        "events": events
+        "events": events,
+        "customers": customers,
+
     }
     return render(request, 'web/index.html', context=context)
